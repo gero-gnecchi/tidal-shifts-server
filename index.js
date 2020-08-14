0f69de396363bfda2137d7420ba23bf567de9801
iamgero <gerolamognecchi@gmail.com>
2020-08-14 11:30:45 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+// express framwork for a basic http server
+//var app = require('express')();
+const express = require('express');
+const app = express();
+// create the http server
+//var http = require('http').createServer(app);
+// require the socket.io and bind it to a port
+
+const server = app.listen(process.env.PORT || 3005,listen);
+
+function listen(){
+
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at http://' + host + ':' + port);
+
+}
+
+
+/*io.attach(http, {
+ pingInterval: 10000,
+ pingTimeout: 5000,
+ cookie: false
+});*/
+
+app.use(express.static('public'));
+
+const io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+
+  console.log('user connected');
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+  socket.on('message', function (msg) {
+   console.log("message:" +msg.X+ " " + msg.Y+ " " + msg.Z );
+
+   io.sockets.emit('message', msg);
+  });
+  timeout();
+});
+
+function timeout() {
+  setTimeout(function () {
+   io.emit('reply',"A message from server");
+    timeout();
+  }, 5000);
+}
+
+
+//http.listen();
---
+++ public/script.js
@@ -0,0 +1,41 @@
+ let X = document.getElementById('X');
+ let Y = document.getElementById('Y');
+ let Z = document.getElementById('Z');
+ let socket;
+
+ // socket = io.connect(10.100.112.164:3005);
+ // socket = io.connect(localhost:3000);
+    socket = io();
+
+   socket.on('message',
+   function(msg){
+     console.log('Tidal Shifts');
+     let x = msg.X;
+     X.innerHTML = x;
+     let y = msg.Y;
+     Y.innerHTML = y;
+     let z = msg.Z;
+     Z.innerHTML = z;
+   });
+
+
+
+
+
+////////////////////////////////////////////////////////// only send, emulating arduino///
+ /*b.onclick = function() {
+   console.log('cloc');
+   sendMessage();
+   // let msg = {
+   //   val: 3
+   // };
+   // socket.emit('message', msg)
+ }
+
+////SEND FUNCTION takes and osc adress and single value
+function sendMessage() {
+  let msg = {
+    val: 3
+  };
+  socket.emit('message', msg)
+}*/
