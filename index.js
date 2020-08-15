7ac1cac54715f527d69e049b30e54ae55974de96
iamgero <gerolamognecchi@gmail.com>
2020-08-15 11:56:18 +0200

    push

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 
 // express framwork for a basic http server
 //var app = require('express')();
-const express = require('express');
+const express = require("express");
 const app = express();
 // create the http server
 //var http = require('http').createServer(app);
 // require the socket.io and bind it to a port
 
-const server = app.listen(process.env.PORT || 80,listen);
+const server = app.listen(process.env.PORT || 3005, listen);
 
-function listen(){
-
+function listen() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Listening at http://' + host + ':' + port);
-
+  console.log("Listening at http://localhost:" + port);
 }
 
 /*io.attach(http, {
@@ -23,31 +21,29 @@ function listen(){
  cookie: false
 });*/
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
-io.on('connection', function (socket) {
+io.on("connection", function (socket) {
+  console.log("user connected");
 
-  console.log('user connected');
-
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
+  io.on("disconnect", function () {
+    console.log("user disconnected");
   });
-  socket.on('message', function (msg) {
-   console.log("message:" +msg.X+ " " + msg.Y+ " " + msg.Z );
 
-   io.sockets.emit('message', msg);
+  socket.on("message", (data) => {
+    console.log("server recieved msg", data);
   });
-  timeout();
+
+  socket.on("gyro", (x, y, z) => {
+    console.log("server recieved gyro msg", x, y, z);
+    console.log("server immediately relaying data to clients");
+    io.emit("data", x, y, z);
+  });
+
+  let counter = 0;
+  setInterval(() => {
+    io.emit("test", { msg: "server test message", counter: counter++ });
+  }, 8000);
 });
-
-function timeout() {
-  setTimeout(function () {
-   io.emit('reply',"A message from server");
-    timeout();
-  }, 5000);
-}
-
-
-//http.listen();
